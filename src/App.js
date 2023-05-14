194eae5955f3632dfd270012fa3093458baefc4e
ony <ogbuebele.ony@hotmail.com>
2023-05-14 22:13:21 +0200

    Add API new guest func

--- src/App.js
+++ src/App.js
@@ -8,7 +8,24 @@ export default function App() {
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
 
+  const baseUrl = 'http://localhost:4000';
+
   const lastNameInputRef = useRef(null);
+
+  async function newGuest() {
+    const response = await fetch(`${baseUrl}/guests`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        firstName: guests.firstName,
+        lastName: guests.lastName,
+      }),
+    });
+    const createdGuest = await response.json();
+    setGuests([...guests, createdGuest]);
+  }
 
   function handleFirstNameChange(event) {
     setFirstName(event.currentTarget.value);
@@ -51,7 +68,7 @@ export default function App() {
       <div>
         <form onSubmit={handleSubmit}>
           <label>
-            First name :
+            First name
             <input
               name="firstName"
               value={firstName}
