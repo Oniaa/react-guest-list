972ef66f1aa6c5df3d1dfb2639c0f329faf70acc
ony <ogbuebele.ony@hotmail.com>
2023-05-16 17:26:21 +0200

    Add comment, rename and clean code

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ export default function App() {
 
   const baseUrl = 'http://localhost:4000';
 
+  // Fetches all submitted Guests from the API on first render
   useEffect(() => {
     async function fetchGuests() {
       const response = await fetch(`${baseUrl}/guests`);
@@ -22,6 +23,7 @@ export default function App() {
     });
   }, []);
 
+  // Adds new Guest to the API and puts the createdGuest in a copy of the guests array
   async function addNewGuest() {
     const response = await fetch(`${baseUrl}/guests`, {
       method: 'POST',
@@ -34,6 +36,7 @@ export default function App() {
     setGuests([...guests, createdGuest]);
   }
 
+  // Lets User enter First and Last Name in the input
   function handleFirstNameChange(event) {
     setFirstName(event.currentTarget.value);
   }
@@ -41,6 +44,8 @@ export default function App() {
     setLastName(event.currentTarget.value);
   }
 
+  // If the Input field is not empty => uses the addNewGuest function to submit the new Guest
+  // and sets the First Name and Last Name input fields to empty again
   async function handleSubmit(event) {
     event.preventDefault();
     if (firstName.trim() !== '' && lastName.trim() !== '') {
@@ -50,23 +55,30 @@ export default function App() {
     }
   }
 
+  // Deletes the Guest from the Screen and the API by making a copy of guest and storing it in currentGuests
+  // filtering through the currentGuests
+  // finding the guest.id that doesnt match the deletedGuest.id and storing them in nonDeletedGuest
   function handleRemoveGuest(id) {
     async function deleteGuest() {
       const response = await fetch(`${baseUrl}/guests/${id}`, {
         method: 'DELETE',
       });
       const deletedGuest = await response.json();
-      const currentGuest = [...guests];
-      const newGuest = currentGuest.filter(
+      const currentGuests = [...guests];
+      const nonDeletedGuest = currentGuests.filter(
         (guest) => guest.id !== deletedGuest.id,
       );
-      setGuests(newGuest);
+      setGuests(nonDeletedGuest);
     }
     deleteGuest().catch((error) => {
       console.error(error);
     });
   }
 
+  // Updates the guests attending status in API and Screen
+  // Creates copy of guests and storing it in currentGuestsStatus filtering through it
+  // finding guest that id doesnt match the updatedGuest.id
+  // setting guests to the oldGuestStatus and the updatedGuestStatus so it still shows all the guests
   async function handleAttendingStatus(id, attending) {
     const response = await fetch(`${baseUrl}/guests/${id}`, {
       method: 'PUT',
@@ -76,11 +88,11 @@ export default function App() {
       body: JSON.stringify({ attending: !attending }),
     });
     const updatedGuest = await response.json();
-    const currentGuestStatus = [...guests];
-    const newGuestStatus = currentGuestStatus.filter(
+    const currentGuestsStatus = [...guests];
+    const oldGuestStatus = currentGuestsStatus.filter(
       (guest) => guest.id !== updatedGuest.id,
     );
-    setGuests([...newGuestStatus, updatedGuest]);
+    setGuests([...oldGuestStatus, updatedGuest]);
     console.log(guests);
   }
 
